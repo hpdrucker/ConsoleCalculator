6c4b967ca2da7100ddcb03c6f71899bf3f59ea4d
Drucker <nicolas.amberg@lernende.bbw.ch>
2021-06-02 11:37:23 +0200

    Test Calculator class with unittest

---
+++ src/test/java/ch/bbw/consolecalculator/CalculatorTest.java
@@ -0,0 +1,15 @@
+package ch.bbw.consolecalculator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+    Calculator testee;
+
+    @Test
+    public void testSummeZweiPositiveIsOk() {
+        testee = new Calculator();
+        assertTrue(testee.summe(10,25) == 35);
+    }
+}
